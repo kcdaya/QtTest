7b2882f394cf7ffc964ea1ebcdbf9e5d0057e0b8
kcdaya <kcdaya@yahoo.com>
2014-07-30 16:03:13 -0500

    update build script

--- build.sh
+++ build.sh
@@ -1,21 +1,31 @@
-#!/bin/bash -ex
+#!/bin/bash -e
 
 echo "#############################################"
 echo " Building QtTest"
 echo "#############################################"
 
-export PATH=/home/dayanand/MySDK/Qt/5.0.2/gcc/bin:$PATH
-export BUILD=RELEASE
 export PLATFORM=linuxX86
+
+if [ $PLATFORM == 'linuxX86' ]; then
+    if [ -d '/opt/qt5-vmwx86' ]; then
+        export PATH=/opt/qt5-vmwx86/bin:$PATH
+    else
+        export PATH=/home/dayanand/MySDK/Qt/5.1.1/gcc/bin:$PATH
+    fi
+else
+    export PATH=/opt/qt5-arm/bin:$PATH
+fi
+
+export BUILD=RELEASE
 
 export workspace=$(pwd)
 cd $workspace
 
-qmake -r test1.pro CONFIG+=" qt5 release " &&
+qmake -r test1.pro CONFIG+=" qt5 release " QT+=" widgets " &&
 make clean &&
 make 2>&1 | tee compile.log
 
-if [ $? -ne 0 ]; then
+if [ $? -ne 1 ]; then
     echo "Build failed due to error!"
     exit 1
 fi
